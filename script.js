96cd3f23759151dd6fe3018989ea6ddf74d652b0
houssamch155-ops <houssamch155@gmail.com>
2026-05-04 22:12:35 +0100

    Initialize birthday messages and display function
    Added a function to display the main content and an array of birthday messages.

---
+++ script.js
@@ -0,0 +1,41 @@
+function start() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("main").classList.remove("hidden");
+}
+const letters = [
+`Happy Birthday, Israee 💜
+
+Today isn’t just another day…  
+It’s the day someone truly special was born.
+
+And I’m grateful for that every single moment.`,
+
+`I hope today reminds you of how loved you are.
+
+Not just today…  
+But every single day.
+
+Because you deserve that and more.`,
+
+`You’ve been strong through things people don’t even see.
+
+And still… you keep going.
+
+That’s something I admire more than words can explain.`,
+
+`On your birthday, I don’t just wish you happiness…
+
+I wish you peace.  
+Confidence.  
+And a future that feels exactly right for you.`,
+
+`And no matter what happens…
+
+I want you to remember this:
+
+You’re not alone.  
+You never were.  
+And you never will be.
+
+Happy Birthday again, Israee 💜`
+];
